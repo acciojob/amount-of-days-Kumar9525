7a92735e38385f8b68d183ec2e5ce88c37abacf4
Kumar9525 <92426822+Kumar9525@users.noreply.github.com>
2024-02-01 20:09:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+
+  const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+
+  return isLeapYear ? 366 : 365;
+}
+console.log(daysOfAYear(2022));
+console.log(daysOfAYear(2024));
+console.log(daysOfAYear(1900));
+console.log(daysOfAYear(2000));
